2f25966d071ab89305d680fe9db561ef5c3458d5
skin547 <skin547yo@gmail.com>
2019-10-16 01:48:59 +0800

    add post method

--- app.py
+++ app.py
@@ -2,24 +2,12 @@ from flask import Flask, render_template
 from flask_restful import Resource, Api
 import time
 import json
-import models.Region
+from models.Region import Region, RegionController
 
 app = Flask(__name__, template_folder="client/build",
             static_folder="client/build/static")
+            
 api = Api(app)
-
-class Region(Resource):
-    def __init__(self):
-        self.regions = [{"name": "Region A", "date": time.time(), "counted": 112}, {
-            "name": "Region B", "date": time.time(), "counted": 26}]
-
-    def get(self):
-        return self.regions
-        
-
-@app.route('/prediction')
-def predict():
-    return "This is test api"
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -28,7 +16,7 @@ def catch_all(path):
     return render_template('index.html')
     # return 'You want path: %s' % path
 
-api.add_resource(Region, '/regions')
+api.add_resource(RegionController, '/regions')
 
 if __name__ == '__main__':
     app.run(debug=True)
