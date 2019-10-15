bd6519f3dbba3c4e83e90e46d5db518a61987874
skin547 <skin547yo@gmail.com>
2019-10-15 12:31:34 +0800

    test get regoins

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
+import time
+import json
+import models.Region
 
 app = Flask(__name__, template_folder="client/build",
             static_folder="client/build/static")
+api = Api(app)
+
+class Region(Resource):
+    def __init__(self):
+        self.regions = [{"name": "Region A", "date": time.time(), "counted": 112}, {
+            "name": "Region B", "date": time.time(), "counted": 26}]
+
+    def get(self):
+        return self.regions
 
 @app.route('/prediction')
 def predict():
@@ -14,5 +27,9 @@ def catch_all(path):
     return render_template('index.html')
     # return 'You want path: %s' % path
 
+
+
+api.add_resource(Region, '/regions')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
