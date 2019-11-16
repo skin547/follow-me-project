5c8d0f7bd91746ef55ea93aa32513b053bc8df74
skin547 <skin547yo@gmail.com>
2019-11-16 19:18:22 +0800

    set something up

--- App/config.py
+++ App/config.py
@@ -10,7 +10,9 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = True
 
 class HerokuConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'postgres://zedkxxduqmtjgk:37eed2f1e3997e1690806a7ad79e077d762e5c6d4ed8546cb0447494970404f0@ec2-54-83-202-132.compute-1.amazonaws.com:5432/daee2gu6ugu8i2'
+    import os
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # SQLALCHEMY_DATABASE_URI = 'postgres://zedkxxduqmtjgk:37eed2f1e3997e1690806a7ad79e077d762e5c6d4ed8546cb0447494970404f0@ec2-54-83-202-132.compute-1.amazonaws.com:5432/daee2gu6ugu8i2'
 
 configs = {'default': BaseConfig,
         'Testing' : TestingConfig,
