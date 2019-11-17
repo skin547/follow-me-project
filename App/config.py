66bcc029278838d88a0839bb3ee7c9424f9dabd6
skin547 <skin547yo@gmail.com>
2019-11-17 14:48:49 +0800

    edit config

--- App/config.py
+++ App/config.py
@@ -1,15 +1,19 @@
 class BaseConfig(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/followmeproject'
 
 class TestingConfig(BaseConfig):
     TESTING = True
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/followmeproject'
     DEBUG = True
 
+class HerokuConfig(BaseConfig):
+    import os
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    
 configs = {'default': BaseConfig,
         'Testing' : TestingConfig,
         'development':DevelopmentConfig,
-        }
+        'heroku': HerokuConfig}
--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from App.config import configs
-
-
-class HerokuConfig(configs['default']):
-    import os
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
 from App import app
 
 if __name__ == '__main__':
-    app.config.from_object(HerokuConfig)
+    app.config.from_object(configs['heroku'])
     app.run(debug=True, threaded=True)
