b2be8ea2af2b6d0310ad03ff10f20bb549f82e73
skin547 <skin547yo@gmail.com>
2019-11-21 09:36:45 +0800

    edit config

--- App/apis/areaApi.py
+++ App/apis/areaApi.py
@@ -37,22 +37,3 @@ class areaApi(Resource):
         db.session.commit()
         return 201
 
-    # def compute_status(self,area):
-    #     if(area.capacity == 0):
-    #         return None
-    #     area_frame = frame.query.filter(frame.area == area.id)
-    #     if(area_frame != None):
-    #         latest_frame = area_frame.order_by(frame.id.desc()).first()
-    #         current_num_of_people = latest_frame.number
-    #         print(current_num_of_people)
-    #         congestion = current_num_of_people/area.capacity
-    #         if(congestion <0.25):
-    #             status = 'green'
-    #         elif(congestion < 0.5):
-    #             status = 'yellow'
-    #         elif(congestion < 0.75):
-    #             status = 'red'
-    #         else:
-    #             status = 'purple'
-    #         return {"congestion":status,"number":current_num_of_people}
-    #     return None
